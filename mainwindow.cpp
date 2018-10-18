fc9cbd7bcb60770532fa89b845c520cbaef55b82
coolxer <lukasik4789@gmail.com>
2018-10-18 14:16:36 +0200

    created basic communication pages in application

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,31 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->statesStackedWidget->setCurrentIndex(0);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_addNewButton_clicked()
+{
+    ui->statesStackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::on_finalAddButton_clicked()
+{
+    ui->statesStackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::on_cancelAddingButton_clicked()
+{
+    ui->statesStackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_editingQuitButton_clicked()
+{
+    ui->statesStackedWidget->setCurrentIndex(0);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,18 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_addNewButton_clicked();
+
+    void on_finalAddButton_clicked();
+
+    void on_cancelAddingButton_clicked();
+
+    void on_editingQuitButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
 };
 
 #endif // MAINWINDOW_H
